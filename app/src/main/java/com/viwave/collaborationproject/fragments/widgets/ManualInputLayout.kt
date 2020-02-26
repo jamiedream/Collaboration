9c7deb0b63bfd9df19d094d1f2d39b6a891351c2
JamieY <jamie.yen@viwave.com>
2020-02-26 18:03:15 +0800

    component: manualinputlayout

---
+++ app/src/main/java/com/viwave/collaborationproject/fragments/widgets/ManualInputLayout.kt
@@ -0,0 +1,46 @@
+package com.viwave.collaborationproject.fragments.widgets
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.viwave.collaborationproject.R
+
+class ManualInputLayout: ConstraintLayout {
+
+    constructor(context: Context): super(context){
+        init(context, null)
+    }
+
+    constructor(context: Context, attributeSet: AttributeSet): super(context, attributeSet){
+        init(context, attributeSet)
+    }
+
+    constructor(context: Context, attributeSet: AttributeSet, defStyleAttr: Int): super(context, attributeSet, defStyleAttr){
+        init(context, attributeSet)
+    }
+
+    private val titleMeasure by lazy { findViewById<TextView>(R.id.title_measurement) }
+    private val unitMeasure by lazy { findViewById<TextView>(R.id.unit_measurement) }
+    private val editMeasure by lazy { findViewById<EditText>(R.id.value_measurement) }
+    private fun init(context: Context, attributesSet: AttributeSet?){
+        View.inflate(context, R.layout.layout_manual_input, this)
+
+        when(attributesSet != null){ true -> {
+            context.theme.obtainStyledAttributes(
+                attributesSet, R.styleable.ManualInput, 0, 0).apply {
+                val title = getString(R.styleable.ManualInput_manualInputTitle)
+                val unit = getString(R.styleable.ManualInput_manualInputUnit)
+                val ime = getInt(R.styleable.ManualInput_manualInputEditIme, 0)
+                val type = getInt(R.styleable.ManualInput_manualInputEditType, 0)
+                titleMeasure.text = title
+                unitMeasure.text = unit
+                editMeasure.imeOptions = ime
+                editMeasure.inputType = type
+                recycle()
+            }
+        }}
+    }
+}
