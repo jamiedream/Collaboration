12e18902a038a42ea136a45d08e5eea6e35391fe
JamieY <jamie.yen@viwave.com>
2020-03-06 13:08:00 +0800

    update device fragment layout

--- app/src/main/java/com/viwave/collaborationproject/DB/cache/DeviceKey.kt
+++ app/src/main/java/com/viwave/collaborationproject/DB/cache/DeviceKey.kt
@@ -1,4 +1,13 @@
 package com.viwave.collaborationproject.DB.cache
 
 object DeviceKey {
+    const val DEVICE_SKU_BP_X3:String = "RM_BP_X3"
+    const val DEVICE_SKU_BP_X5:String = "RM_BP_X5"
+    const val DEVICE_SKU_TM_HC700:String = "RM_TM_HC700"
+    const val DEVICE_SKU_BG_HT100:String = "RM_BG_HT100"
+    const val DEVICE_SKU_BG_HS200:String = "RM_BG_HS200"
+    const val DEVICE_SKU_OXIMETER_SA300:String = "RM_PO_SA300"
+    const val DEVICE_SKU_OXIMETER_SA310:String = "RM_PO_SA310"
+    const val DEVICE_SKU_OXIMETER_SB210:String = "RM_PO_SB210"
+    const val DEVICE_SKU_FAT_SCALE_LS212B:String = "RM_BF_LS212-B"
 }
--- app/src/main/java/com/viwave/collaborationproject/fragments/DeviceFragment.kt
+++ app/src/main/java/com/viwave/collaborationproject/fragments/DeviceFragment.kt
@@ -4,16 +4,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import com.viwave.collaborationproject.R
 
 class DeviceFragment: BaseFragment() {
+
+    private val TAG = this::class.java.simpleName
+
+    private val txtMacBP by lazy { view!!.findViewById<TextView>(R.id.mac_bp) }
+    private val txtMacBG by lazy { view!!.findViewById<TextView>(R.id.mac_bg) }
+    private val txtMacTemp by lazy { view!!.findViewById<TextView>(R.id.mac_temp) }
+    
+    private val btnBindBP by lazy { view!!.findViewById<Button>(R.id.btn_device_bp) }
+    private val btnBindBG by lazy { view!!.findViewById<Button>(R.id.btn_device_bg) }
+    private val btnBindTemp by lazy { view!!.findViewById<Button>(R.id.btn_device_temp) }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.fragment_device, container, false)
     }
 
     override fun onResume() {
