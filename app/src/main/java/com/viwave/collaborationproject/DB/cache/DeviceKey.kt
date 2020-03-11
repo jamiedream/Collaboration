c31facb8f91c562f892e40bed065874bc253739f
JamieY <jamie.yen@viwave.com>
2020-03-11 17:46:09 +0800

    update device key table

--- app/src/main/java/com/viwave/collaborationproject/DB/cache/DeviceKey.kt
+++ app/src/main/java/com/viwave/collaborationproject/DB/cache/DeviceKey.kt
@@ -2,15 +2,12 @@ package com.viwave.collaborationproject.DB.cache
 
 object DeviceKey {
 
-    const val DEVICE_SKU_BP_X3:String = "RM_BP_X3"
-    const val DEVICE_SKU_BP_X5:String = "RM_BP_X5"
-    const val DEVICE_SKU_TM_HC700:String = "RM_TM_HC700"
-    const val DEVICE_SKU_BG_HT100:String = "RM_BG_HT100"
-    const val DEVICE_SKU_BG_HS200:String = "RM_BG_HS200"
-    const val DEVICE_SKU_OXIMETER_SA300:String = "RM_PO_SA300"
-    const val DEVICE_SKU_OXIMETER_SA310:String = "RM_PO_SA310"
-    const val DEVICE_SKU_OXIMETER_SB210:String = "RM_PO_SB210"
-    const val DEVICE_SKU_FAT_SCALE_LS212B:String = "RM_BF_LS212-B"
+    const val DEVICE_SKU_BP_X3 = "RM_BP_X3"
+    const val DEVICE_SKU_BP_X5 = "RM_BP_X5"
+    const val DEVICE_SKU_TM_HC700 = "RM_TM_HC700"
+    const val DEVICE_SKU_BG_HT100 = "RM_BG_HT100"
+    const val DEVICE_SKU_OXIMETER_SB210 = "RM_PO_SB210"
+    const val DEVICE_SKU_FAT_SCALE_LS212B = "RM_BF_LS212-B"
 
     const val DEVICE_KEY_BP = "Blood Pressure Monitor"
     const val DEVICE_KEY_BG = "Blood Glucose Meters"
@@ -21,6 +18,7 @@ object DeviceKey {
     const val DEVICE_X3 = "X3"
     const val DEVICE_X5 = "X5"
     const val DEVICE_HT100 = "HT100"
-    const val DEVICE_HS200 = "HS200"
     const val DEVICE_HC700 = "HC700"
+    const val DEVICE_SB210 = "SB210"
+    const val DEVICE_LS212B = "LS212B"
 }
