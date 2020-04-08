3f86ec52349cd6017bb1e0354e3b36e5897470a4
JamieY <jamie.yen@viwave.com>
2020-04-08 10:53:39 +0800

    change framework of history

--- app/src/main/java/com/viwave/collaborationproject/fragments/subsys/history/HistoryFragment.kt
+++ app/src/main/java/com/viwave/collaborationproject/fragments/subsys/history/HistoryFragment.kt
@@ -8,10 +8,9 @@ package com.viwave.collaborationproject.fragments.subsys.history
 
 import android.os.Bundle
 import android.view.*
-import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.appcompat.widget.PopupMenu
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import com.viwave.collaborationproject.DB.remote.entity.CaseEntity
 import com.viwave.collaborationproject.FakeData.QueryData
@@ -19,6 +18,7 @@ import com.viwave.collaborationproject.R
 import com.viwave.collaborationproject.data.DataSort
 import com.viwave.collaborationproject.data.bios.BioLiveData
 import com.viwave.collaborationproject.fragments.BaseFragment
+import com.viwave.collaborationproject.fragments.subsys.caseList.CaseListFragment
 import com.viwave.collaborationproject.fragments.subsys.caseList.CaseListFragment.Companion.bioViewModel
 import com.viwave.collaborationproject.fragments.subsys.caseList.CaseListFragment.Companion.caseViewModel
 import com.viwave.collaborationproject.utils.ConvertUtil
@@ -27,10 +27,13 @@ class HistoryFragment: BaseFragment(){
 
     private val TAG = this::class.java.simpleName
 
-    private val textDiagramType by lazy { view!!.findViewById<TextView>(R.id.diagram_type) }
-    private val imgDiagramMenu by lazy { view!!.findViewById<ImageView>(R.id.diagram_menu) }
-    private val imgDiagramChartList by lazy { view!!.findViewById<ImageView>(R.id.diagram_chart_list) }
-    private val contentLayout by lazy { view!!.findViewById<FrameLayout>(R.id.content_history) }
+    private val caseGender by lazy { view!!.findViewById<ImageView>(R.id.case_gender) }
+    private val caseName by lazy { view!!.findViewById<TextView>(R.id.case_name) }
+    private val caseNumber by lazy { view!!.findViewById<TextView>(R.id.case_number) }
+
+    private val CHART = "CHART"
+    private val LIST = "LIST"
+    private var PAGE = MutableLiveData<String>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -42,138 +45,127 @@ class HistoryFragment: BaseFragment(){
         return view
     }
 
-    override fun onPrepareOptionsMenu(menu: Menu) {
-        val item = menu.findItem(R.id.dashboard_no)
-        caseViewModel.getSelectedCase().value?.let {
-            item.setTitle(it.getCaseNumber)
-        }
-        super.onPrepareOptionsMenu(menu)
-    }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
-        inflater.inflate(R.menu.menu_dashboard_no, menu)
+        when(PAGE.value){
+            LIST -> inflater.inflate(R.menu.menu_history_list, menu)
+            CHART -> inflater.inflate(R.menu.menu_history_chart, menu)
+        }
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return when(item.itemId){
+                    R.id.menu_history_chart -> {
+                        PAGE.value = LIST
+                        true
+                    }
+                    R.id.menu_history_list -> {
+                        PAGE.value = CHART
+                        true
+                    }
+                    else -> super.onOptionsItemSelected(item)
+                }
+    }
+
+    private val pageObserver =
+        Observer<String>{
+            activity?.invalidateOptionsMenu()
+            when(it){
+                LIST -> replacePartialFragment(this, HistoryListFragment(), R.id.content_history, getString(R.string.tag_list))
+                CHART -> replacePartialFragment(this, HistoryChartFragment(), R.id.content_history, getString(R.string.tag_chart))
+            }
+        }
 
     private val selectedCaseObserver =
         Observer<CaseEntity>{
-            setToolbarTitle(it.getCaseName)
-            setToolbarLeftIcon(false)
+            caseName.text = it.getCaseName
+            caseNumber.text = it.getCaseNumber
+            caseGender.setImageResource(
+                when(it.getCaseGender){
+                    CaseListFragment.FEMALE -> R.drawable.ic_gender_female
+                    else -> R.drawable.ic_gender_male
+                }
+            )
         }
 
     private val selectedTypeObserver =
         Observer<BioLiveData.Companion.BioType>{
-            textDiagramType.text = getString(bioViewModel.getSelectedTypeHistoryTitle().value?: R.string.temperature)
-
             when(it){
                 BioLiveData.Companion.BioType.Temperature -> {
+                    setToolbarTitle(getString(R.string.temperature))
                     bioViewModel.getTempListData().value =
                         ConvertUtil.sortListData(DataSort.temperatureList, QueryData().tempData)
                     bioViewModel.getTempListData().value?.sortBy { it.takenAt }
                 }
 
                 BioLiveData.Companion.BioType.Pulse -> {
+                    setToolbarTitle(getString(R.string.pulse))
                     bioViewModel.getPulseListData().value =
                         ConvertUtil.sortListData(DataSort.pulseList, QueryData().pulseData)
                     bioViewModel.getPulseListData().value?.sortBy { it.takenAt }
                 }
 
                 BioLiveData.Companion.BioType.Weight -> {
+                    setToolbarTitle(getString(R.string.weight))
                     bioViewModel.getWeightListData().value =
                         ConvertUtil.sortListData(DataSort.weightList, QueryData().weightData)
                     bioViewModel.getWeightListData().value?.sortBy { it.takenAt }
                 }
 
                 BioLiveData.Companion.BioType.BloodGlucose -> {
+                    setToolbarTitle(getString(R.string.blood_glucose))
                     bioViewModel.getGlucoseListData().value =
                         ConvertUtil.sortListData(DataSort.glucoseList, QueryData().glucoseData)
                     bioViewModel.getGlucoseListData().value?.sortBy { it.takenAt }
                 }
 
                 BioLiveData.Companion.BioType.BloodPressure -> {
+                    setToolbarTitle(getString(R.string.blood_pressure))
                     bioViewModel.getBPListData().value =
                         ConvertUtil.sortListData(DataSort.bloodPressureList, QueryData().bpData)
                     bioViewModel.getBPListData().value?.sortBy { it.takenAt }
                 }
 
                 BioLiveData.Companion.BioType.Oxygen -> {
+                    setToolbarTitle(getString(R.string.oxygen))
                     bioViewModel.getOxygenListData().value =
                         ConvertUtil.sortListData(DataSort.oxygenList, QueryData().oxygenData)
                     bioViewModel.getOxygenListData().value?.sortBy { it.takenAt }
                 }
             }
 
-            when(imgDiagramChartList.tag){
-                getString(R.string.tag_list) -> replacePartialFragment(this, HistoryListFragment(), R.id.content_history, getString(R.string.tag_list))
-                getString(R.string.tag_chart) -> replacePartialFragment(this, HistoryChartFragment(), R.id.content_history, getString(R.string.tag_chart))
+            when(PAGE.value){
+                LIST -> replacePartialFragment(this, HistoryListFragment(), R.id.content_history, getString(R.string.tag_list))
+                CHART -> replacePartialFragment(this, HistoryChartFragment(), R.id.content_history, getString(R.string.tag_chart))
             }
-
 
         }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        //init
+        PAGE.value = LIST
 
         caseViewModel.getSelectedCase().observe(this, selectedCaseObserver)
 
-        //init
-        replacePartialFragment(this,
-            HistoryListFragment(), R.id.content_history, getString(R.string.tag_list))
-
         bioViewModel.getSelectedType().observe(this, selectedTypeObserver)
+
+        PAGE.observe(this, pageObserver)
 
     }
 
     override fun onResume() {
         super.onResume()
+        setToolbarLeftIcon(false)
 
-        //switch popup menu
-        imgDiagramMenu.setOnClickListener {
-
-            PopupMenu(view!!.context, it).apply {
-                this.menuInflater.inflate(R.menu.menu_chart, menu)
-                this.show()
-                this.setOnMenuItemClickListener { item ->
-                    bioViewModel.getSelectedType().value =
-                        when (item.title) {
-                            getString(R.string.temperature) -> BioLiveData.Companion.BioType.Temperature
-                            getString(R.string.pulse) -> BioLiveData.Companion.BioType.Pulse
-                            getString(R.string.respire) -> BioLiveData.Companion.BioType.Respire
-                            getString(R.string.blood_pressure) -> BioLiveData.Companion.BioType.BloodPressure
-                            getString(R.string.blood_glucose) -> BioLiveData.Companion.BioType.BloodGlucose
-                            getString(R.string.height) -> BioLiveData.Companion.BioType.Height
-                            getString(R.string.weight) -> BioLiveData.Companion.BioType.Weight
-                            getString(R.string.oxygen) -> BioLiveData.Companion.BioType.Oxygen
-                            else -> BioLiveData.Companion.BioType.Temperature
-                        }
-                    true
-                }
-
-            }
-        }
-
-        //switch chart and list
-        imgDiagramChartList.setOnClickListener {
-            when(imgDiagramChartList.tag){
-                getString(R.string.tag_list) -> {
-                    imgDiagramChartList.tag = getString(R.string.tag_chart)
-                    imgDiagramChartList.isPressed = true
-                    imgDiagramChartList.setImageResource(R.drawable.ic_baseline_menu)
-                    replacePartialFragment(this,
-                        HistoryChartFragment(), R.id.content_history, getString(R.string.tag_chart))
-                }
-                getString(R.string.tag_chart) -> {
-                    imgDiagramChartList.tag = getString(R.string.tag_list)
-                    imgDiagramChartList.isPressed = false
-                    imgDiagramChartList.setImageResource(R.drawable.ic_health_chart)
-                    replacePartialFragment(this,
-                        HistoryListFragment(), R.id.content_history, getString(R.string.tag_list))
-                }
-            }
-        }
     }
 
     override fun onStop() {
+        PAGE.removeObserver(pageObserver)
+        bioViewModel.getSelectedType().removeObserver(selectedTypeObserver)
+        caseViewModel.getSelectedCase().removeObserver(selectedCaseObserver)
         super.onStop()
     }
 
